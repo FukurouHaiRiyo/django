458af42b35271ab51235730e2e1ec89d2d922916
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2023-02-16 20:44:51 +0200

    Update settings.py

--- project/myscrummy/myscrummy/settings.py
+++ project/myscrummy/myscrummy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u@-rv7^ayjn8%rw^wb#814hp3sq*ab4$ttcvoi*mld%5kain*t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
